12830c4632f9caf7448b14a4281b6bda4afaf2b6
Sadidadri <porerotote7@gmail.com>
2023-02-03 20:39:01 +0100

    scripts folder

---
+++ Paejam2023/Assets/Scripts/FPSController.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FPSController : MonoBehaviour
+{
+    CharacterController controller;
+
+
+    public float walkSpeed = 6f;
+    public float runSpeed = 10f;
+    public float jumpSpeed = 8f;
+    public float gravity = 20f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<>()
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
